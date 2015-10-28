acc00dc7e12804928ad32d663bd3ecc2275aefd4
Tim Combs <tim00combs@gmail.com>
2015-10-27 21:54:12 -0700

    3rd commit - changed comment in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -21,19 +21,6 @@ gulp.task('watch', function() {
 
 gulp.task('default', ['lint', 'errorTest', 'watch']);
 
-/*
-For this assignment you will add a gulpfile and a package.json file to your previous assignment.
-
-The package.json file should include all the dependencies and dev dependencies for the project.
-
-The gulpfile should contain a task to run the mocha/chai test as well as run jshint on all of your code. This should include your tests and your gulpfile.
-
-Submit as a pull request to your own repo that contains just the code for this assignment (gulpfile, package.json and jshint config file if applicable).
 
 
-
-Bonus:
-For an extra point, set up a watch task that reruns your tests/jshint on changes to any of your files (minus package.json)
-
-For another bonus point move the jshint options (node and globals) into a jshintrc file that you can transfer between projects.
-*/
+// may refactor later to move the jshint options (node and globals) into a jshintrc file that you can transfer between projects.
